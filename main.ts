153146ec26c83b4d343a1e10e546f392ed67353e
Itti Srisumalai <i2ktel@hotmail.com>
2019-12-30 16:14:37 +0700

    Updates.

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.AB, function () {
     loraBit.reset()
     loraBit.param_OTAA(
-    "0008420454B8BE23",
-    "70B3D57ED00219AA",
-    "10D33F399DB357CDE2BF02599222E28D"
+        "0008420454B8BE23",
+        "70B3D57ED00219AA",
+        "10D33F399DB357CDE2BF02599222E28D"
     )
     loraBit.join(loraBit_freq_Plan.AS923)
     basic.showString("join lorabit_demo_002")
@@ -33,6 +33,9 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     sos = true
 })
+input.onPinPressed(TouchPin.P0, function () {
+    sos = true
+})
 let result = 0
 let sos = false
 led.setBrightness(20)
@@ -40,16 +43,15 @@ basic.showIcon(IconNames.Happy)
 sos = false
 let interval = input.runningTime()
 loraBit.Verbose(Verbose_Mode.On)
-cayenneLPP.add_digital(LPP_Direction.Input_Port, DigitalPin.P0)
 cayenneLPP.add_digital(LPP_Direction.Output_Port, DigitalPin.P1)
 cayenneLPP.add_analog(LPP_Direction.Input_Port, AnalogPin.P2)
 cayenneLPP.add_sensor(LPP_Bit_Sensor.Temperature)
 cayenneLPP.add_sensor(LPP_Bit_Sensor.Light)
 cayenneLPP.add_sensor(LPP_Bit_Sensor.LED_Brightness)
 loraBit.param_Config(
-5,
-7,
-loraBit_ADR.On
+    5,
+    7,
+    loraBit_ADR.On
 )
 basic.clearScreen()
 basic.forever(function () {
